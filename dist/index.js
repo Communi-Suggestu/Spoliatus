d7d12c38e18d90a1091f3a26ae0c6140afa5a3d0
Marc Hermans <marc.hermans@ldtteam.com>
2025-01-10 22:10:32 +0100

    create implementation

--- dist/index.js
+++ dist/index.js
@@ -27239,35 +27239,23 @@ function requireCore () {
 var coreExports = requireCore();
 
 /**
- * Waits for a number of milliseconds.
- *
- * @param milliseconds The number of milliseconds to wait.
- * @returns Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds))
-            throw new Error('milliseconds is not a number');
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
-}
-
-/**
  * The main function for the action.
  *
  * @returns Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = coreExports.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        coreExports.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        coreExports.debug(new Date().toTimeString());
-        await wait(parseInt(ms, 10));
-        coreExports.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        coreExports.setOutput('time', new Date().toTimeString());
+        const version = coreExports.getInput('version');
+        const parts = version.split('.');
+        if (parts.length < 1) {
+            coreExports.setFailed(`Invalid version format: ${version}`);
+            return;
+        }
+        const minor = version[1];
+        const patch = version.length > 1 ? version[2] : 0;
+        coreExports.setOutput('minor', minor);
+        coreExports.setOutput('patch', patch);
+        coreExports.setOutput('joined', `${minor}.${patch}`);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
